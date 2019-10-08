7e8f2bb68979349063743d0d23f8d2d86fbc8747
TheBeastLT <pauliox@beyond.lt>
2019-10-08 10:25:32 +0200

    updates addon sdk

--- addon.js
+++ addon.js
@@ -7,7 +7,11 @@ const { cacheWrapStream } = require('./lib/cache');
 
 const IS_LOCAL = !(process.env.LOCAL_NETWORK === 'false' || process.env.LOCAL_NETWORK === '0');
 const PROVIDERS = [new vidsrc.Provider(), new vidstreaming.Provider(), new wonderfulsubs.Provider()];
-const CACHE_MAX_AGE = process.env.CACHE_MAX_AGE || 43200;
+
+const CACHE_MAX_AGE = process.env.CACHE_MAX_AGE || 2 * 24 * 60; // 2 days
+const CACHE_MAX_AGE_EMPTY = 4 * 60; // 4 hours in seconds
+const STALE_REVALIDATE_AGE = 4 * 60; // 4 hours
+const STALE_ERROR_AGE = 7 * 24 * 60; // 7 days
 
 const builder = new addonBuilder({
   id: 'com.stremio.cdn.videos' + (IS_LOCAL ? '.local' : ''),
@@ -34,7 +38,12 @@ builder.defineStreamHandler((args) => {
   };
 
   return cacheWrapStream(args.id, handlers[args.type] || handlers.fallback)
-    .then((streams) => ({ streams: streams, cacheMaxAge: CACHE_MAX_AGE }))
+    .then((streams) => ({
+      streams: streams,
+      cacheMaxAge: streams.length ? CACHE_MAX_AGE : CACHE_MAX_AGE_EMPTY,
+      staleRevalidate: STALE_REVALIDATE_AGE,
+      staleError: STALE_ERROR_AGE
+    }))
     .catch((error) => {
       console.log(`Failed request ${args.id}: ${error}`);
       throw error;
